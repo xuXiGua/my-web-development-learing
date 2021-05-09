6f82119367bf7bbbcfad0a46376f2defb668a6f1
xuXiGua <82155142+xuXiGua@users.noreply.github.com>
2021-05-09 20:32:15 +0800

    Update myjs.js

--- OPPOwebsite/js/myjs.js
+++ OPPOwebsite/js/myjs.js
@@ -12,32 +12,92 @@ function setBanner() {
     const btn2 = document.querySelector('#banBtn2');
     const btn3 = document.querySelector('#banBtn3');
     const btn4 = document.querySelector('#banBtn4');
+    const btnle = document.querySelector('#banl');
+    const btnr = document.querySelector('#banr');
 
     const image1 = document.querySelector('#banner1');
     const image2 = document.querySelector('#banner2');
     const image3 = document.querySelector('#banner3');
     const image4 = document.querySelector('#banner4');
 
+    let index = 1;
+
     function allDisappear(){
         image1.style.opacity = '0';
         image2.style.opacity = '0';
         image3.style.opacity = '0';
         image4.style.opacity = '0';
+        if(btn1.classList.contains('active')){
+            btn1.classList.remove('active');
+        }
+        if(btn2.classList.contains('active')){
+            btn2.classList.remove('active');
+        }
+        if(btn3.classList.contains('active')){
+            btn3.classList.remove('active');
+        }
+        if(btn4.classList.contains('active')){
+            btn4.classList.remove('active');
+        }
     }
-    function picAppear(img) {
+
+    function showPic(i) {
+        console.log(index);
         allDisappear();
-        img.style.opacity = '1';
-        // this.classList.add('active');
+        switch (i) {
+            case 1:
+                image1.style.opacity = '1';
+                btn1.classList.add('active');
+                index = 1;
+                break;
+            case 2:
+                image2.style.opacity = '1';
+                btn2.classList.add('active');
+                index = 2;
+                break;
+            case 3:
+                image3.style.opacity = '1';
+                btn3.classList.add('active');
+                index = 3;
+                break;
+            case 4:
+                image4.style.opacity = '1';
+                btn4.classList.add('active');
+                index = 4;
+        }
     }
 
-    btn1.addEventListener('click', function(){picAppear(image1)});
-    btn2.addEventListener('click', function(){picAppear(image2)});
-    btn3.addEventListener('click', function(){picAppear(image3)});
-    btn4.addEventListener('click', function(){picAppear(image4)});
+    function dealIndex(i){
+        if(i < 1 ){
+            i = 4;
+        } else if (i > 4){
+            i = 1;
+        }
+        return i;
+    }
 
-    // setTimeout(function(){picAppear(image2)},3000);
-    // setTimeout(function(){picAppear(image3)},3000);
+    btn1.addEventListener('click', function(){showPic(1)});
+    btn2.addEventListener('click', function(){showPic(2)});
+    btn3.addEventListener('click', function(){showPic(3)});
+    btn4.addEventListener('click', function(){showPic(4)});
+    
+    btnle.addEventListener('click', function(){
+        index--;console.log(index);
+        index = dealIndex(index);console.log(index);
+        showPic(index);
+    });
+    btnr.addEventListener('click', function(){
+        index++;
+        index = dealIndex(index);
+        showPic(index);
+    });
 
+    setInterval(function(){
+        index++;
+        index = dealIndex(index);
+        showPic(index);
+        console.log(index);
+    }, 5000);
 }
 
 
